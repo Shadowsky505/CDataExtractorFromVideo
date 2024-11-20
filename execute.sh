7eb50e3b286a4844babc02aef3cd06e095e4a145
Ayrtoon Tintaya <ayrtoon.tintaya@tecsup.edu.pe>
2024-11-19 23:21:05 -0500

    Commit

--- execute.sh
+++ execute.sh
@@ -1,11 +1,10 @@
-#!/bin/bash
 apt update
 apt install docker.io docker-compose -y
-# Solicitar la API Key
+
 read -p "Ingresa tu API Key: " API_KEY
 
-# Crear y ejecutar el contenedor con la variable de entorno
-docker build -t vosk-api .
-docker run -d -p 8000:8000 --name vosk-api-container -e OPEN_API_KEY="$API_KEY" vosk-api
+export API_KEY
 
-echo "El contenedor se está ejecutando en el puerto 8000."
+docker-compose up --build -d
+
+echo "El contenedor 'vosk-api-container' está corriendo en el puerto 8000."
